5b6444e62fe473b46f2b57f9bded4b01d26dce1d
sababab299 <sbregvadze2011@gmail.com>
2025-06-24 15:50:49 +0400

    Quit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,10 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
     }
 }
